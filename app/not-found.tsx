a6609d8cf08cc08d986a70ce9b0f5ef98ff5b040
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2024-04-23 18:01:20 +0800

    feat: landing page and roadmap

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <div className="my-auto p-8 md:p-0">
+      <Image
+        src="/undraw_empty_re_opql.svg"
+        width={500}
+        height={500}
+        alt="resource not found"
+      />
+      <p className="my-2 text-center text-teal-500">
+        Looks like we could not find what you are looking for!
+      </p>
+    </div>
+  );
+}
